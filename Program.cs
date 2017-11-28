b8670bfc447745de6bedf18e5230454736152f47
LiZeC <tomgreem123@gmail.com>
2017-11-28 15:08:30 +0800

    hello world

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace LibraryinfoSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            string s = "Hello World";
         }
     }
 }
